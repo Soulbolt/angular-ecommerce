e806c688e7154df816920018a60e0358ced465e4
Soulbolt <soulbolt@gmail.com>
2024-02-28 09:23:02 -0700

    Add routing paths for each Category and handle redirects in app.routing component. Add routerLinks to Books and Coffe mugs in the app.component.html for now. Add varaible currentCategoryId, add route  subscribtion for product list. Declare const boolean variable to hold Id . Check if not null to get id else assign it number 1. Pass the currentCategoryId to getProductList in product-list.component and varaible: number in prodcut.service.

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { Product } from '../../common/product';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
@@ -9,16 +10,34 @@ import { Product } from '../../common/product';
 })
 export class ProductListComponent implements OnInit {
   products: Product[] = [];
+  currentCategoryId: number = 1;
 
-  constructor(private productService: ProductService) {}
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute,
+  ) {}
 
   ngOnInit(): void {
-    this.listProducts();
-  }
-
-  listProducts() {
-    this.productService.getProductList().subscribe((data) => {
-      this.products = data;
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
     });
+  }
+  listProducts() {
+    // Check if "id" parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if (hasCategoryId) {
+      // Get the "id" param string. Convert string to a number using the "+" symbol. the "!" is a non-null assertion operator.
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+    } else {
+      // Not category id available... default to category id 1
+      this.currentCategoryId = 1;
+    }
+
+    this.productService
+      .getProductList(this.currentCategoryId)
+      .subscribe((data) => {
+        this.products = data;
+      });
   }
 }
