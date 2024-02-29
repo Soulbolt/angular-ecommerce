e3e3852d12a2571cbfd3685d3da54b3be0ed3d42
Soulbolt <soulbolt@gmail.com>
2024-02-28 22:43:44 -0700

    Refactor by adding listProducts code into a handleListProducts method. Add route.snapshot to get keyword inside listProducts method. Add a if else to cehck for searchMode(searchMode boolean added at top level) subscribe to productService method to return data.

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -12,6 +12,7 @@ export class ProductListComponent implements OnInit {
   products: Product[] = [];
   currentCategoryId: number = 1;
   currentCategoryName: string = '';
+  searchMode: boolean = false;
 
   constructor(
     private productService: ProductService,
@@ -24,6 +25,24 @@ export class ProductListComponent implements OnInit {
     });
   }
   listProducts() {
+    this.route.snapshot.paramMap.has('keyword');
+
+    if (this.searchMode) {
+      this.handleSearchProducts();
+    } else {
+      this.handleListProducts();
+    }
+  }
+
+  handleSearchProducts() {
+    const theKeyword: string = this.route.snapshot.paramMap.get('keyword')!;
+    // Search for the products using keyword
+    this.productService.searchProducts(theKeyword).subscribe((data) => {
+      this.products = data;
+    });
+  }
+
+  handleListProducts() {
     // Check if "id" parameter is available
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
 
