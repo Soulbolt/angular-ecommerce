ed3810953294438116b4fb74513fa11afa14df12
Soulbolt <soulbolt@gmail.com>
2024-02-29 08:33:30 -0700

    Add product variable, inject ActivatedRoute and ProductService. Add listerner for handleProductDetails method in ngOnInit. Add handleProductDetails method. assign const to get current id, Add code to fetch getProduct for product details.

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../common/product';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrl: './product-details.component.css'
+  styleUrl: './product-details.component.css',
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+  product!: Product;
 
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService,
+  ) {}
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(() => {
+      this.handleProductDetails();
+    });
+  }
+
+  handleProductDetails() {
+    const theProductId: number = +this.route.snapshot.paramMap.get('id')!;
+
+    this.productService.getProduct(theProductId).subscribe((data) => {
+      this.product = data;
+    });
+  }
 }
