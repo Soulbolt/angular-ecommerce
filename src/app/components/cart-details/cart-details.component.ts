7fc2fe56dad3bbfafb92f568638da943660bc175
Soulbolt <soulbolt@gmail.com>
2024-03-01 20:00:38 -0700

    Add carItems, totalPrice, and totalQuantity variables to cart-details.component. Inject CartService. Add listCartDetails method and declare cartItems variable.  subscribe to TotalPrice and totalQuantity, compute cart total price and quantity. Call it in ngOnInit.

--- src/app/components/cart-details/cart-details.component.ts
+++ src/app/components/cart-details/cart-details.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartItem } from '../../common/cart-item';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-cart-details',
   templateUrl: './cart-details.component.html',
-  styleUrl: './cart-details.component.css'
+  styleUrl: './cart-details.component.css',
 })
-export class CartDetailsComponent {
+export class CartDetailsComponent implements OnInit {
+  cartItems: CartItem[] = [];
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
 
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+    this.listCartDetails();
+  }
+
+  listCartDetails() {
+    // Get a handle ot the cart items
+    this.cartItems = this.cartService.cartItems;
+
+    // Subscribe to cart totalPrrice
+    this.cartService.totalPrice.subscribe((data) => (this.totalPrice = data));
+
+    // Subscribe to cart totalQuantity
+    this.cartService.totalQuantity.subscribe(
+      (data) => (this.totalQuantity = data),
+    );
+
+    // Compute cart total Price and Quantity
+    this.cartService.computeCartTotals();
+  }
 }
