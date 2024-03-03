0c54c9fc6e38ad26222d86d49d620eed0ad6eef1
Soulbolt <soulbolt@gmail.com>
2024-03-03 13:17:30 -0700

    Add countriesUrl and statesUrl endpoint variables, Add getCountries and getState methods. Add interface GetResponseCountires and GetResponseStates in luv2-shop-form.service.

--- src/app/services/luv2-shop-form.service.ts
+++ src/app/services/luv2-shop-form.service.ts
@@ -1,11 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { map } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root',
 })
 export class Luv2ShopFormService {
-  constructor() {}
+
+  private countriesUrl = "http://localhost:8080/api/countries";
+  private statesUrl = "http://localhost:8080/api/states";
+
+  constructor(private httpClient: HttpClient) {}
 
   getCreditCardMonths(startMonth: number): Observable<number[]> {
     let data: number[] = [];
@@ -32,5 +38,34 @@ export class Luv2ShopFormService {
     }
 
     return of(data);
+  }
+
+  getCountries(): Observable<Country[]> {
+    return this.httpClient.get<GetResponseCountries>(this.countriesUrl).pipe(
+      map((response) => response._embedded.countries)
+    );
+  }
+
+  getStates(theCountryCode: string): Observable<State[]> {
+    const searchStatesUrl = `${this.statesUrl}/search/findByCountryCode?code=${theCountryCode}`;
+
+    return this.httpClient.get<GetResponseStates>(searchStatesUrl).pipe(
+      map((response) => {
+        return response._embedded.states;
+      })
+    )
+  }
+
+  interface GetResponseCountries {
+    _embedded: {
+      id: number;
+      countries: Country[];
+    }
+  }
+
+  interface GetResponseStates {
+    _embedded: {
+      states: State[];
+    }
   }
 }
