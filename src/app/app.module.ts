a54266f91d831069387b4dfb89088ff409b8ebf8
Soulbolt <soulbolt@gmail.com>
2024-03-02 18:36:46 -0700

    Generate checkout.component. Add path for checkout for CheckoutComponent in the app-routing.module.ts. Add a tag with routerLink to route to "/checkout". Add and wrap a div with class main-content around the p tag line.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { ProductDetailsComponent } from './components/product-details/product-de
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CartStatusComponent } from './components/cart-status/cart-status.component';
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { CartDetailsComponent } from './components/cart-details/cart-details.com
     ProductDetailsComponent,
     CartStatusComponent,
     CartDetailsComponent,
+    CheckoutComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, NgbModule],
   providers: [ProductService],
