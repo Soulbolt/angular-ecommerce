93967efb80a85d69e5fd3fe775ba96eb74517790
Soulbolt <soulbolt@gmail.com>
2024-03-05 12:22:19 -0700

    Add my-app-config.ts add oidc object with clientId, issuer, redirectUri, and scopes. declare okta information accordingly.

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+  oidc: {
+    clientId: '0oa1s3k0vqy6rJvY15d7',
+    issuer: 'https://dev-853545.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+  },
+};
