c973f718a9c5e50474e09e5923dd3a7211112448
Soulbolt <soulbolt@gmail.com>
2024-03-05 17:28:30 -0700

    Generate login-status component, inject OKTA_AUTH and oktaAuthStateService. Subscribe throuigh oktaAuthService to auth state cahnges in side ngOnInIt. Add getUserDetails Method. Add logout method.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { CartDetailsComponent } from './components/cart-details/cart-details.com
 import { CheckoutComponent } from './components/checkout/checkout.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './components/login/login.component';
+import { LoginStatusComponent } from './components/login-status/login-status.component';
 
 @NgModule({
   declarations: [
@@ -28,6 +29,7 @@ import { LoginComponent } from './components/login/login.component';
     CartDetailsComponent,
     CheckoutComponent,
     LoginComponent,
+    LoginStatusComponent,
   ],
   imports: [
     BrowserModule,
