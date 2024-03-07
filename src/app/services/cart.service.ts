f407eda242903f0e16bc98df0b0a05d01e256161
Soulbolt <soulbolt@gmail.com>
2024-03-07 08:26:34 -0700

    Handle browser refresh by loading and persisting cart items in cart.service.ts

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -11,7 +11,19 @@ export class CartService {
   totalPrice: Subject<number> = new BehaviorSubject<number>(0);
   totalQuantity: Subject<number> = new BehaviorSubject<number>(0);
 
-  constructor() {}
+  storage: Storage = sessionStorage;
+
+  constructor() {
+    // Read data from storage
+    let data = JSON.parse(this.storage.getItem('cartItems'));
+
+    if (data != null) {
+      this.cartItems = data;
+
+      // compute totals based on the data that is read from storage
+      this.computeCartTotals();
+    }
+  }
 
   addToCart(theCartItem: CartItem) {
     // Check if we already have the item in our cart.
@@ -89,6 +101,10 @@ export class CartService {
 
     // Log cart data for debugging purposes
     this.logCartData(totalPriceValue, totalQuantityValue);
+  }
+
+  persistCartItems() {
+    this.storage.setItem('cartItems', JSON.stringify(this.cartItems));
   }
 
   logCartData(totalPriceValue: number, totalQuantityValue: number) {
