ac42795bf32b6fe5dd2020628f5dc049a21d79da
Soulbolt <soulbolt@gmail.com>
2024-03-07 23:02:28 -0700

    Generate auth interceptor service, implement HttpInterceptor, Add interceptor method  and handleAccess method to pass async promise.

---
+++ src/app/services/auth-interceptor.service.ts
@@ -0,0 +1,29 @@
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, from } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthInterceptorService implements HttpInterceptor {
+  constructor() {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler,
+  ): Observable<HttpEvent<any>> {
+    return from(this.handleAccess(req, next));
+  }
+
+  private async handleAccess(
+    req: HttpRequest<any>,
+    next: HttpHandler,
+  ): Promise<HttpEvent<any>> {
+    throw new Error('Method not implemented.');
+  }
+}
