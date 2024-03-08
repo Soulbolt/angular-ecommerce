d755321223640ea7d2ca57c4887add27b78ebf4b
Soulbolt <soulbolt@gmail.com>
2024-03-08 14:10:01 -0700

    Add receiptEmail property to forward receipt to customers email.

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -374,6 +374,7 @@ export class CheckoutComponent implements OnInit {
     // Compute payment info
     this.paymentInfo.amount = Math.round(this.totalPrice * 100);
     this.paymentInfo.currency = 'USD';
+    this.paymentInfo.receiptEmail = purchase.customer.email;
 
     // If valid form then
     // - create payment intent
