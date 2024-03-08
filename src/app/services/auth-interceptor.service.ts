8f330fcac6d687d2d206bc6affa5e5501aefe4ad
Soulbolt <soulbolt@gmail.com>
2024-03-07 23:10:07 -0700

    Inject OKTA_AUTH and OktaAuth into constructor. Add const to add seucireEndpoints to. Check if url  includes securedEndpoints, get accessToken with await getAccessToken method. Clone the qurest and add new header with the token. return the lastValue.

--- src/app/services/auth-interceptor.service.ts
+++ src/app/services/auth-interceptor.service.ts
@@ -4,14 +4,16 @@ import {
   HttpInterceptor,
   HttpRequest,
 } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable, from } from 'rxjs';
+import { Inject, Injectable } from '@angular/core';
+import { OKTA_AUTH } from '@okta/okta-angular';
+import OktaAuth from '@okta/okta-auth-js';
+import { Observable, from, lastValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthInterceptorService implements HttpInterceptor {
-  constructor() {}
+  constructor(@Inject(OKTA_AUTH) private oktaAuth: OktaAuth) {}
 
   intercept(
     req: HttpRequest<any>,
@@ -24,6 +26,21 @@ export class AuthInterceptorService implements HttpInterceptor {
     req: HttpRequest<any>,
     next: HttpHandler,
   ): Promise<HttpEvent<any>> {
-    throw new Error('Method not implemented.');
+    // Only add an access token for secured endpoints
+    const securedEndpoints = ['http://localhost:8080/api/orders'];
+
+    if (securedEndpoints.some((url) => req.urlWithParams.includes(url))) {
+      // Get access token
+      const accessToken = await this.oktaAuth.getAccessToken();
+
+      // Clone the request and add new header with access token
+      req = req.clone({
+        setHeaders: {
+          Authorization: 'Bearer ' + accessToken,
+        },
+      });
+    }
+
+    return await lastValueFrom(next.handle(req));
   }
 }
