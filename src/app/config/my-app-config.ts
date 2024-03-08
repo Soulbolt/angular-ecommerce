9b58534af33b8277b9adf082ca3a049fa8f3d027
Soulbolt <soulbolt@gmail.com>
2024-03-08 08:17:47 -0700

    update package.json to use new ssl-certificate and update myappconfig to use https.

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -2,7 +2,7 @@ export default {
   oidc: {
     clientId: '0oafkiox2uVeveAfz5d7',
     issuer: 'https://dev-68989635.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:4200/login/callback',
+    redirectUri: 'https://localhost:4200/login/callback',
     scopes: ['openid', 'profile', 'email'],
   },
 };
