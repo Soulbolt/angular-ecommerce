b04c7ed1881886faba43bd20080835c2a568ca43
Soulbolt <soulbolt@gmail.com>
2024-03-08 13:54:21 -0700

    Disable purchase button after first action to prevent multiple submissions/purchases.

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -46,6 +46,8 @@ export class CheckoutComponent implements OnInit {
   cardElement: any;
   displayError: any = '';
 
+  isDisabled: boolean = false;
+
   constructor(
     private formBuilder: FormBuilder,
     private luv2ShopFormService: Luv2ShopFormService,
@@ -381,6 +383,8 @@ export class CheckoutComponent implements OnInit {
       !this.checkoutFormGroup.invalid &&
       this.displayError.textContent === ''
     ) {
+      this.isDisabled = true;
+
       this.checkoutService
         .createPaymentIntent(this.paymentInfo)
         .subscribe((paymentIntentResponse) => {
@@ -409,6 +413,7 @@ export class CheckoutComponent implements OnInit {
               if (result.error) {
                 // Inform the customer there was an error
                 alert(`There was an error: ${result.error.message}`);
+                this.isDisabled = false;
               } else {
                 // Call REST API via the CheckoutService
                 this.checkoutService.placeOrder(purchase).subscribe({
@@ -417,9 +422,11 @@ export class CheckoutComponent implements OnInit {
                       `Your order has been received.\nOrder tracking number: ${response.orderTrackingNumber}`,
                     );
                     this.resetCart();
+                    this.isDisabled = false;
                   },
                   error: (err) => {
                     alert(`There was an error: ${err.message}`);
+                    this.isDisabled = false;
                   },
                 });
               }
